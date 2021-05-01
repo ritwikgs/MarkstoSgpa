6dc5b6f32dff88005ecd983e66309959dfee23e0
ritwikgs <43063981+ritwikgs@users.noreply.github.com>
2021-05-01 16:33:10 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Studentsgpa rit = new Studentsgpa("Ritwik");
+        Studentsgpa praj = new Studentsgpa("abc");
+        System.out.println(rit.calsgpa());
+        System.out.println(abc.calsgpa());
+    }
+}
